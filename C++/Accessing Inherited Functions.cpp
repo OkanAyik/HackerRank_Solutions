2c4233327ea19f19946c2b9ee4721402d2466ad7
OkanAyik <okanayik3@gmail.com>
2021-09-13 18:53:12 +0300

    indentation fixing

--- C++/Accessing Inherited Functions.cpp
+++ C++/Accessing Inherited Functions.cpp
@@ -1,37 +1,36 @@
 class D : public A, B, C
 {
+    int val;
+public:
+    //Initially val is 1
+    D()
+    {
+        val = 1;
+    }
 
-	int val;
-	public:
-		//Initially val is 1
-		 D()
-		 {
-		 	val = 1;
-		 }
 
-
-		 //Implement this function
-		 void update_val(int new_val)
-		 {
-             int a = new_val;
-             
-            while(a%2 == 0){
-                a = a/2;
-                A::func(val);
-            } 
+    //Implement this function
+    void update_val(int new_val)
+    {
+        int a = new_val;
+          
+        while(a%2 == 0){
+            a = a/2;
+            A::func(val);
+        } 
             
-            while(a%3 == 0){
-                a = a/3;
-                B::func(val);
-            }
+        while(a%3 == 0){
+            a = a/3;
+            B::func(val);
+        }
             
-            while(a%5 == 0){
-                a = a/5;
-                C::func(val);
-            }
+        while(a%5 == 0){
+            a = a/5;
+            C::func(val);
+        }
             
 			
-		 }
-		 //For Checking Purpose
-		 void check(int); //Do not delete this line.
+    }
+    //For Checking Purpose
+    void check(int); //Do not delete this line.
 };
--- C++/C++ Class Template Specialization.cpp
+++ C++/C++ Class Template Specialization.cpp
@@ -1,37 +1,30 @@
 // Define specializations for the Traits class template here.
 template<typename T>
 struct Traits {
-    Traits() = default;
+	Traits() = default;
 
-    static string name(int val)
-    {
-        if (is_same<T, Color>::value) {
-            switch (val) {
-            case 0:
-                return string{ "red" };
-            case 1:
-                return string{ "green" };
-            case 2:
-                return string{ "orange" };
-            default:
-                return string{ "unknown" };
-            }
-        }
-        else if(is_same<T, Fruit>::value){
-            switch (val) {
-            case 0:
-                return string{"apple"};
-            case 1:
-                return string{"orange"};
-            case 2:
-                return string{"pear"};
-            default:
-                return string{"unknown"};
-            }
-        }
-        else {
-            return string{ "unknown" };
-        }
-    }
+	static string name(int val)
+	{
+		if (is_same<T, Color>::value) {
+			switch (val) {
+				case 0: return string{ "red" };
+				case 1: return string{ "green" };
+				case 2: return string{ "orange" };
+				default: return string{ "unknown" };
+			}
+		}
 
+		else if(is_same<T, Fruit>::value){
+			switch (val) {
+				case 0: return string{"apple"};
+				case 1: return string{"orange"};
+				case 2: return string{"pear"};
+				default: return string{"unknown"};
+			}
+		}
+
+		else {
+			return string{ "unknown" };
+		}
+	}
 };
